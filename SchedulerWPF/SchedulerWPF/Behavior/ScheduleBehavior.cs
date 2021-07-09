a6e1651417e1abe2bebadbc3ab4ec6f72614d2a1
SaiGanesh Sakthivel <saiganesh.sakthivel@syncfusion.com>
2021-07-09 15:25:34 +0530

    Sample is added

---
+++ SchedulerWPF/SchedulerWPF/Behavior/ScheduleBehavior.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SchedulerWPF
+{
+    public class ScheduleBehavior : Behavior<Grid>
+    {
+        Grid grid;
+        SfScheduler schedule;
+        protected override void OnAttached()
+        {
+            grid = this.AssociatedObject as Grid;
+            schedule = grid.Children[0] as SfScheduler;
+            schedule.AppointmentEditorOpening += OnAppointmentEditorOpening;
+        }
+
+        private void OnAppointmentEditorOpening(object sender, AppointmentEditorOpeningEventArgs e)
+        {
+            e.AppointmentEditorOptions = AppointmentEditorOptions.All | (~AppointmentEditorOptions.Background & ~AppointmentEditorOptions.Foreground & ~AppointmentEditorOptions.Reminder & ~AppointmentEditorOptions.Resource);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            grid = null;
+            schedule = null;
+        }
+    }
+}
---
+++ SchedulerWPF/SchedulerWPF/Viewmodel/SchedulerViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SchedulerWPF
+{
+    public class SchedulerViewModel : INotifyPropertyChanged
+    {
+        private ScheduleAppointmentCollection events;
+        public SchedulerViewModel()
+        {
+            this.Events = new ScheduleAppointmentCollection();
+            var scheduleAppointment = new ScheduleAppointment()
+            {
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddHours(1),
+                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF339933")),
+                Subject = "Conference",
+                Reminders = new ObservableCollection<SchedulerReminder>
+                {
+                    new SchedulerReminder { ReminderTimeInterval = new TimeSpan(0)},
+                }
+            };
+            Events.Add(scheduleAppointment);
+        }
+                
+        public ScheduleAppointmentCollection Events
+        {
+            get
+            {
+                return this.events;
+            }
+            set
+            {
+                this.events = value;
+                this.RaiseOnPropertyChanged("ScheduleAppointmentCollection");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
